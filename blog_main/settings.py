21ff9087505b04680680f5fda9174a8a761a780f
Swarup Dash <swarupsdash21@gmail.com>
2026-06-09 17:59:04 +0530

    Database changes commit

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -85,8 +85,9 @@ WSGI_APPLICATION = 'blog_main.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
+    "default": dj_database_url.config(
+        default="postgresql://postgres:swarup21@localhost:5432/blog_db",
+        conn_max_age=600,
     )
 }
 
