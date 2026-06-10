7733615621805e5bbb782fe5e2a75bb80d1424da
Swarup Dash <swarupsdash21@gmail.com>
2026-06-10 08:18:51 +0530

    change in build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,13 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+User.objects.filter(username='adminswarup').exists() or User.objects.create_superuser(
+'adminswarup',
+'[swarupsekhar21@gmail.com](mailto:swarupsekhar21@gmail.com)',
+'sekharlizun21'
+)
+"
